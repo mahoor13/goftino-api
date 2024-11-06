f20f62e47f407b104d7a1c4c76b6848220ac03ce
mahoor13 <git@oop.ir>
2024-11-06 17:06:24 +0330

    complete all api methods

--- src/GoftinoApi.php
+++ src/GoftinoApi.php
@@ -13,10 +13,14 @@ class GoftinoApi
      * __construct
      *
      * @param  string $api_key
+     * @param  int $log_level
+     * 
      * @return void
      */
-    public function __construct(string $api_key)
-    {
+    public function __construct(
+        string $api_key,
+        public int $log_level = 0,
+    ) {
         $this->factory($api_key);
     }
 
@@ -24,6 +28,7 @@ class GoftinoApi
      * factory
      *
      * @param  string $api_key
+     * 
      * @return void
      */
     public function factory(string $api_key)
@@ -40,24 +45,36 @@ class GoftinoApi
     /**
      * call
      * 
-     * @param string $api
-     * @param string $request_method
-     * @param array $options
+     * @param string $function
+     * @param array $params
      * 
      * @return mixed
      */
-    public function call(string $api, string $request_method = 'get', array $options = []): mixed
+    public function call(string $function, array $params = []): mixed
     {
-        $options = array_filter($options, fn($val) => $val !== null, ARRAY_FILTER_USE_BOTH);
-        $response = $request_method === 'get'
-            ? $this->client->get($api, ['query' => $options])
-            : $this->client->post($api, ['json' => $options]);
+        [$request_method, $api] = explode('_', $function, 2);
 
-        return json_decode($response->getBody()->getContents(), true);
+        if ($this->log_level > 0) {
+            echo "\n\nAPI:\t{$request_method} -> {$api}\n";
+            echo "Params:\t" . json_encode($params, JSON_UNESCAPED_UNICODE) . "\n";
+        }
+
+        $params = array_filter($params, fn($val) => $val !== null, ARRAY_FILTER_USE_BOTH);
+        $response = strtolower($request_method) === 'get'
+            ? $this->client->get($api, ['query' => $params])
+            : $this->client->post($api, ['json' => $params]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        if ($this->log_level > 0) {
+            echo "Result:\t" . json_encode($result, JSON_UNESCAPED_UNICODE) . "\n";
+        }
+
+        return $result;
     }
 
     /**
-     * chats
+     * get_chats
      * 
      * @param int $limit
      * @param int $page
@@ -67,13 +84,13 @@ class GoftinoApi
      * 
      * @return mixed
      */
-    public function chats(int $limit = 50, int $page = 1, ?string $operator_id = null, string $status = null, ?bool $has_owner = null)
+    public function get_chats(int $limit = 50, int $page = 1, ?string $operator_id = null, string $status = null, ?bool $has_owner = null)
     {
-        return $this->call(__FUNCTION__, 'get', compact('limit', 'page', 'operator_id', 'status', 'has_owner'));
+        return $this->call(__FUNCTION__, compact('limit', 'page', 'operator_id', 'status', 'has_owner'));
     }
 
     /**
-     * chat_data
+     * get_chat_data
      *
      * @param string $chat_id
      * @param string|null $from_date
@@ -83,8 +100,232 @@ class GoftinoApi
      * 
      * @return mixed
      */
-    public function chat_data(string $chat_id, ?string $from_date = null, ?string $to_date = null, int $limit = 50, int $page = 1)
+    public function get_chat_data(string $chat_id, ?string $from_date = null, ?string $to_date = null, int $limit = 50, int $page = 1)
     {
-        return $this->call(__FUNCTION__, 'get', compact('chat_id', 'from_date', 'to_date', 'limit', 'page'));
+        return $this->call(__FUNCTION__, compact('chat_id', 'from_date', 'to_date', 'limit', 'page'));
+    }
+
+    /**
+     * post_send_message
+     *
+     * @param string $chat_id
+     * @param string $operator_id
+     * @param string $message
+     * @param string|null $reply_id
+     * 
+     * @return mixed
+     */
+    public function post_send_message(string $chat_id, string $operator_id, string $message, ?string $reply_id = null)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id', 'operator_id', 'message', 'reply_id'));
+    }
+
+    /**
+     * post_send_message_from_user
+     *
+     * @param string $chat_id
+     * @param string $message
+     * @param string|null $reply_id
+     * 
+     * @return mixed
+     */
+    public function post_send_message_from_user(string $chat_id, string $message, ?string $reply_id = null)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id', 'message', 'reply_id'));
+    }
+
+    /**
+     * post_operator_typing
+     *
+     * @param string $chat_id
+     * @param string $operator_id
+     * @param string $typing_status
+     * 
+     * @return mixed
+     */
+    public function post_operator_typing(string $chat_id, string $operator_id, string $typing_status = 'true')
+    {
+        return $this->call(__FUNCTION__, compact('chat_id', 'operator_id', 'typing_status'));
+    }
+
+    /**
+     * post_close_chat
+     *
+     * @param string $chat_id
+     * @param string $operator_id
+     * 
+     * @return mixed
+     */
+    public function post_close_chat(string $chat_id, string $operator_id)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id', 'operator_id'));
+    }
+
+    /**
+     * post_transfer_chat
+     *
+     * @param string $chat_id
+     * @param string $from_operator
+     * @param string $to_operator
+     * 
+     * @return mixed
+     */
+    public function post_transfer_chat(string $chat_id, string $from_operator, string $to_operator)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id', 'from_operator', 'to_operator'));
+    }
+
+    /**
+     * post_unassign_chat
+     *
+     * @param string $chat_id
+     * @param string $from_operator
+     * 
+     * @return mixed
+     */
+    public function post_unassign_chat(string $chat_id, string $from_operator)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id', 'from_operator'));
+    }
+
+    /**
+     * get_edit_message
+     *
+     * @param string|null $message_id
+     * 
+     * @return mixed
+     */
+    public function get_edit_message(string $message_id = null)
+    {
+        return $this->call(__FUNCTION__, compact('message_id'));
+    }
+
+    /**
+     * post_edit_message
+     *
+     * @param string $message
+     * @param string $message_id
+     * 
+     * @return mixed
+     */
+    public function post_edit_message(string $message, string $message_id)
+    {
+        return $this->call(__FUNCTION__, compact('message', 'message_id'));
+    }
+
+    /**
+     * post_send_poll
+     *
+     * @param string $chat_id
+     * 
+     * @return mixed
+     */
+    public function post_send_poll(string $chat_id)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id'));
+    }
+
+    /**
+     * create_chat
+     *
+     * @param string $message
+     * 
+     * @return mixed
+     */
+    public function post_create_chat(string $message)
+    {
+        return $this->call(__FUNCTION__, compact('message'));
+    }
+
+    /**
+     * post_remove_chat
+     *
+     * @param string $chat_id
+     * 
+     * @return mixed
+     */
+    public function post_remove_chat(string $chat_id)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id'));
+    }
+
+    /**
+     * get_user_data
+     *
+     * @param string|null $chat_id
+     * @param string|null $user_id
+     * 
+     * @return mixed
+     */
+    public function get_user_data(?string $chat_id = null, ?string $user_id = null)
+    {
+        return $this->call(__FUNCTION__, compact('chat_id', 'user_id'));
+    }
+
+    /**
+     * post_user_data
+     *
+     * @param string|null $user_id
+     * @param string|null $name
+     * @param string|null $phone
+     * @param string|null $email
+     * @param string|null $avatar
+     * @param string|null $description
+     * @param array $tags
+     * @param mixed 
+     * 
+     * @return mixed
+     */
+    public function post_user_data(
+        ?string $user_id = null,
+        ?string $name = null,
+        ?string $phone = null,
+        ?string $email = null,
+        ?string $avatar = null,
+        ?string $description = null,
+        array $tags = [],
+    ) {
+        return $this->call(__FUNCTION__, compact('user_id', 'name', 'phone', 'email', 'avatar', 'description', 'tags'));
+    }
+
+    /**
+     * post_ban_user
+     *
+     * @param string $operator_id
+     * @param string|null $chat_id
+     * @param string|null $user_id
+     * 
+     * @return mixed
+     */
+    public function post_ban_user(string $operator_id, ?string $chat_id = null, ?string $user_id = null)
+    {
+        return $this->call(__FUNCTION__, compact('operator_id', 'chat_id', 'user_id'));
+    }
+
+    /**
+     * get_user_visited_pages
+     *
+     * @param string|null $user_id
+     * 
+     * @return mixed
+     */
+    public function get_user_visited_pages(?string $user_id = null)
+    {
+        return $this->call(__FUNCTION__, compact('user_id'));
+    }
+
+    /**
+     * get_operators
+     *
+     * @return mixed
+     */
+    public function get_operators()
+    {
+        return $this->call(__FUNCTION__);
+    }
+
+    public function operator_data(?string $email = null, ?string $operator_id = null)
+    {
+        return $this->call(__FUNCTION__);
     }
 }
--- tests/GoftinoApiTest.php
+++ tests/GoftinoApiTest.php
@@ -13,10 +13,10 @@ class GoftinoApiTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->api = new GoftinoApi(getenv('GOFTINO_TEST_API_KEY') ?: $this->apiKey);
+        $this->api = new GoftinoApi(getenv('GOFTINO_TEST_API_KEY') ?: $this->apiKey, 1);
     }
 
-    public function testChatsThrowsExceptionOnError()
+    public function testGetChatsThrowsExceptionOnError()
     {
         // Set an invalid API key to trigger an error response
         $this->api->factory('INVALID_API_KEY_TEST');
@@ -25,18 +25,21 @@ class GoftinoApiTest extends TestCase
         // But Goftino API returns http status code 200 !!! Why? I don't know!
         // $this->expectException(\GuzzleHttp\Exception\RequestException::class);
 
-        $result = $this->api->chats();
+        $result = $this->api->get_chats();
 
         // Assert that the result matches the expected response
         $this->assertIsArray($result);
         $this->assertEquals("error", $result["status"]);
         $this->assertEquals("1", $result["code"]);
+
+        // rollback api key
+        $this->api->factory(getenv('GOFTINO_TEST_API_KEY') ?: $this->apiKey);
     }
 
-    public function testChatsReturnsExpectedData()
+    public function testGetChatsReturnsExpectedData()
     {
         // Call the chats method
-        $result = $this->api->chats(limit: 2);
+        $result = $this->api->get_chats(limit: 2);
 
         // Assert that the result matches the expected response
         $this->assertIsArray($result);
@@ -52,19 +55,77 @@ class GoftinoApiTest extends TestCase
     }
 
     /**
-     * @depends testChatsReturnsExpectedData
+     * @depends testGetChatsReturnsExpectedData
      */
-    public function testChatDataReturnsExpectedData($chatId)
+    public function testGetChatDataReturnsExpectedData($chatId)
     {
-        $this->assertNotNull($chatId, 'chat_id is not set from previous test');
+        $this->assertNotNull($chatId, 'chat_id is not set from previous test (chats)');
 
-        // Call the chats method and set chat_id from testChatsReturnsExpectedData method
-        $result = $this->api->chat_data(chat_id: $chatId);
+        // Call the chat_data method and set chat_id from testGetChatsReturnsExpectedData method
+        $result = $this->api->get_chat_data(chat_id: $chatId);
 
         // Assert that the result matches the expected response
         $this->assertIsArray($result);
         $this->assertEquals("success", $result["status"]);
         $this->assertIsArray($result["data"]);
         $this->assertIsArray($result["data"]["messages"]);
+    }
+
+    public function testPostCreateChatReturnsExpectedData()
+    {
+        $result = $this->api->post_create_chat("Message 1 (create chat)");
+
+        // Assert that the result matches the expected response
+        $this->assertIsArray($result);
+        $this->assertEquals("success", $result["status"]);
+        $this->assertIsArray($result["data"]);
+        $this->assertIsString($result["data"]["chat_id"]);
+        $this->assertIsString($result["data"]["user_id"]);
+
+        return $result["data"];
+    }
+
+    /**
+     * @depends testPostCreateChatReturnsExpectedData
+     */
+    public function testPostSendMessageFromUserReturnsExpectedData(array $data)
+    {
+        $this->assertNotNull($data['chat_id'], 'chat_id is not set from previous test (create_chat)');
+        $result = $this->api->post_send_message_from_user($data['chat_id'], "Message 2 (send message from user)");
+
+        // Assert that the result matches the expected response
+        $this->assertIsArray($result);
+        $this->assertEquals("success", $result["status"]);
+        $this->assertIsArray($result["data"]);
+        $this->assertIsString($result["data"]["message_id"]);
+        $data['message_id'] = $result["data"]["message_id"];
+
+        return $data;
+    }
+
+    /**
+     * @depends testPostSendMessageFromUserReturnsExpectedData
+     */
+    public function testPostSendMessageReturnsExpectedData(array $data)
+    {
+        $this->assertNotNull($data['chat_id'], 'user_id is not set from previous test (create_chat)');
+        $result = $this->api->post_send_message($data['chat_id'], "671f20cb03abe02f50018dff",  "Hi there 👋", $data['message_id'] ?? null);
+
+        // Assert that the result matches the expected response
+        $this->assertIsArray($result);
+        $this->assertEquals("success", $result["status"]);
+        $this->assertIsArray($result["data"]);
+        $this->assertIsString($result["data"]["message_id"]);
+    }
+
+    public function testGetOperatorsReturnsExpectedData()
+    {
+        $result = $this->api->get_operators();
+
+        // Assert that the result matches the expected response
+        $this->assertIsArray($result);
+        $this->assertEquals("success", $result["status"]);
+        $this->assertIsArray($result["data"]);
+        $this->assertIsArray($result["data"]["operators"]);
     }
 }
